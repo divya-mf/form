47b4bce3fc4a7f20323e1467428de864697d039d
Divya N <dv19996@gmail.com>
2018-08-10 16:07:12 +0530

    minor changes in js

--- validation.js
+++ validation.js
@@ -3,18 +3,24 @@ function validateForm() {
     var fname = document.getElementById("fname").value;
 	var lname = document.getElementById("lname").value;
     if (fname == "") {
-        var msg ="Name must be filled out";
+        var msg ="Fill in your first name";
 		 document.getElementById("msg").innerHTML = msg;
 		 form.fname.focus();
 		 //document.getElementById('fname').style.border ="1px solid red";
         return false;
     }
 	if (lname == "") {
-        var msg ="Name must be filled out";
+        var msg ="fill in your last name";
 		 document.getElementById("msg").innerHTML = msg;
 		 form.lname.focus();
         return false;
     }
+	if( /^[a-zA-Z\s]+$/.test( fname ) == false || /^[a-zA-Z\s]+$/.test( lname ) == false ) {
+        var msg ="Invalid name";
+		document.getElementById("msg").innerHTML = msg;
+        return false;
+    }
+	
 	var db = document.getElementById("db").value;
 	if (db == "") {
         var msg ="Fill in your date of birth";
@@ -30,6 +36,16 @@ function validateForm() {
 		 form.email.focus();
         return false;
     }
+	var at = email.indexOf("@");
+    var dot = email.lastIndexOf(".");
+         
+         if (at < 1 || ( dot - at < 2 )) 
+         {
+            var msg = "Please enter a valid email ID";
+			document.getElementById("msg").innerHTML = msg;
+            form.email.focus() ;
+            return false;
+         }
 	
 	var pw = document.getElementById("pw").value;
 	var pw_c = document.getElementById("pw_confirm").value;
@@ -48,16 +64,7 @@ function validateForm() {
 		
 	}
 	
-	var at = email.indexOf("@");
-    var dot = email.lastIndexOf(".");
-         
-         if (at < 1 || ( dot - at < 2 )) 
-         {
-            var msg = "Please enter valid email ID";
-			document.getElementById("msg").innerHTML = msg;
-            form.email.focus() ;
-            return false;
-         }
+	
 	
 	
 	
